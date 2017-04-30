94d4af35e743cc9f6e6b0e4f8b2949c1ce529075
dandanthedevman <danielkrzyzanowskiphoto@gmail.com>
2017-04-30 01:19:16 -0500

    Finished 5.3

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -9,9 +9,13 @@ hey { |name1, name2| puts "yeah you guys #{name1} and #{name2}"}
 
 
 #Release 1
+
 #Array
+
 skate_array = [ "bluetown", "welcome", "shred Shop", "spitfire"]
+
 #Hash
+
 snowboards_hash = { board: "public", boots: "32", bindings: "union" }
 
 #Print Array and hash 
@@ -21,11 +25,14 @@ snowboards_hash = { board: "public", boots: "32", bindings: "union" }
 #Using .each 
 
 #Hash 
+
 snowboards_hash.each do |type_of_equipment, brand|
 	puts "those are cool #{type_of_equipment}, 
 	#{brand} makes some great stuff. "
 end 
-#Array 
+
+#Array
+
 skate_array.each do |board|
   puts "that's a cool #{board}deck!"
 end
@@ -33,6 +40,7 @@ end
 #Using .map and .map! 
 
 #Normal map
+
 modified_skate = skate_array.map do |board| 
   board.upcase
 end
@@ -42,6 +50,7 @@ p skate_array
 p modified_skate
 
 #Dangerous map
+
 skate_array.map! do |board|
 	board.upcase
 end 
@@ -59,7 +68,7 @@ p array1.delete_if {|num| num < 3 }
 
 p hash1.delete_if {|number, digit| digit < 3 }
 
-#2 keeps any number less than five
+#2. Keeps any number less than five
 	#method .keep_if
 
 array2 = [2, 4, 6, 9, 12, 23]
@@ -70,7 +79,7 @@ p array2.keep_if {|num| num < 5 }
 
 p hash2.keep_if {|number, digit| digit < 5 }
 
-#3 filter out even numbers
+#3. Filter out even numbers
 	#method .select 
 
 array3 = [2, 4, 6, 9, 12, 23]
@@ -82,7 +91,7 @@ p array3.select {|digit| digit.even? }
 p hash3.select {|number, digit| digit.even? }
 
 
-#4 A method that will remove items from a data structure until the condition in the block evaluates to false:
+#4. A method that will remove items from a data structure until the condition in the block evaluates to false:
 	#method .take_while 
 
 array4 = [2, 4, 6, 9, 12, 23]
