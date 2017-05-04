0472620ef715ff80441c7132ffad093d6a750a08
dandanthedevman <danielkrzyzanowskiphoto@gmail.com>
2017-05-04 14:19:30 -0500

    Finished release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,37 @@ class Puppy
     toy
   end
 
-end
+  def speak(int)
+  	int.times {puts "woof"}
+  end 
 
+  def roll_over
+  	"*rolls over*"
+  end
+
+  def dog_years (h_year)
+  	dog_years = h_year * 7
+ 		dog_years
+  end
+  
+  def personal_space (distance)
+  	comfort_d = 3 
+  	if distance < comfort_d 
+  		puts "needy dog"
+    else 
+  		puts "whats wrong bud"
+  	end 
+  end
+
+  end
+puppy = Puppy.new
+
+puppy.fetch("bone")
+
+puppy.speak(4)
+
+p puppy.roll_over
+
+p puppy.dog_years(6)
+
+puppy.personal_space(2)
