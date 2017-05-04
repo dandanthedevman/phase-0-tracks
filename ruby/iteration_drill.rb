14e829ea39dcbd6ef6439a244f37a9883ad52e11
dandanthedevman <danielkrzyzanowskiphoto@gmail.com>
2017-05-03 21:00:37 -0500

    updated wrong awnsers

--- ruby/iteration_drill.rb
+++ ruby/iteration_drill.rb
@@ -51,13 +51,15 @@ find_shotgun(zombie_apocalypse_supplies)
 # leaving only 5. Do not use any special built-in methods.
 # ----
 
-def bare_minimum(array) 
-	loop do 
-		array[0].delete 
-	  break if array.length <= 5
-  end 
+zombie_apocalypse_supplies.each do |x|  
+  zombie_apocalypse_supplies.each do |y|
+    if zombie_apocalypse_supplies.length > 5
+       zombie_apocalypse_supplies.delete(y) 
+    end
+  end  
 end
-p bare_minimum(zombie_apocalypse_supplies)
+
+
 
 # 5. You found another survivor! This means you can combine your supplies.
 # Create a new combined supplies list out of your zombie_apocalypse_supplies
@@ -67,6 +69,7 @@ p bare_minimum(zombie_apocalypse_supplies)
 other_survivor_supplies = [ "warm clothes", "rations", "compass", "camp stove",
                             "solar battery", "flashlight"]
 # ----
+zombie_apocalypse_supplies.concat(other_survivor_supplies).uniq 
 
 # Hash Drills
 
@@ -83,17 +86,21 @@ extinct_animals = {
 # 1. Iterate through extinct_animals hash, printing each key/value pair
 # with a dash in between the key and value, and an asterisk between each pair.
 # ----
+extinct_animals.each {|type, year| puts "#{type} - #{year} *"}
 
 # 2. Keep only animals in extinct_animals if they were extinct before
 # the year 2000. Do not use any special built-in methods.
-# ----
-
+# ---
+extinct_animals.delete_if {|type, year| year >= 2000 }
 # 3. Our calculations were completely off, turns out all of those animals went
 # extinct 3 years before the date provided. Update the values in extinct_animals
 # so they accurately reflect what year the animal went extinct.
 # Do not use any special built-in methods.
 # ----
+updated
 
+extinct_animals.each {|type, year| puts "#{type} - #{year-3 } *"}
+ 
 # 4. You've heard that the following animals might be extinct, but you're not sure.
 # Check if they're included in extinct_animals, one by one:
 # "Andean Cat"
@@ -101,9 +108,29 @@ extinct_animals = {
 # "Saiga Antelope"
 # Do not use any special built-in methods.
 # ----
+extinct_animals.each do |type, year|
+  case 
+  when type == "Andean Cat"
+    puts "#{type} = Animal Extinct"
+  when type == "Dodo"
+    puts "#{type} = Animal Extinct"
+  when type == "Saiga Antelope"
+    puts "#{type} = Animal Extinct"
+  else puts "#{type} = Still around"
+  end
+end 
+
 
 # 5. We just found out that the Passenger Pigeon is actually not extinct!
 # Remove them from extinct_animals and return the key value pair as a two item array.
 # Find the built-in method that helps you accomplish this in the Ruby documentation
 # for Hashes.
 # ----
+extinct_animals.delete_if{|type,year| type != "Passenger Pigeon"}.shift
+
+
+
+
+
+
+
