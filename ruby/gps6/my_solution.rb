6a397d8e8ca5af1e0a3ffdd9e20cd11a823ffa34
dandanthedevman <danielkrzyzanowskiphoto@gmail.com>
2017-05-12 12:46:06 -0500

    finished gps updated my solution

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,11 +1,11 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+#I worked on this challenge [by Adam Weil, Dan Krzyzanowski].
+# We spent [2] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
-#
+# require_relative allows you to bring in objects from the same primary directory that the file is located in
+# require would you be used to pull from the ruby folder directory, rather then a specific path defined
 require_relative 'state_data'
 
 class VirusPredictor
@@ -17,45 +17,37 @@ class VirusPredictor
   end
 
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
 
   private
 
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
-    if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
-    elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
-    elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
-    elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
-    else
-      number_of_deaths = (@population * 0.05).floor
-    end
+    
+    num = case @population_density
+      when (200...99999) then (0.4 * @population).floor
+      when (150...200) then (0.3 * @population).floor
+      when (100...150) then (0.2 * @population).floor
+      when (50...100) then (0.1 * @population).floor
+      else (@population * 0.05).floor 
+    end 
 
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+    print "#{@state} will lose #{num} people in this outbreak"
 
   end
 
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
-
-    if @population_density >= 200
-      speed += 0.5
-    elsif @population_density >= 150
-      speed += 1
-    elsif @population_density >= 100
-      speed += 1.5
-    elsif @population_density >= 50
-      speed += 2
-    else
-      speed += 2.5
+    
+    speed = case @population_density 
+      when (200...99999) then 0.5 
+      when (150...200) then 1
+      when (100...150) then 1.5 
+      when (50...100) then 2 
+      else 2.5 
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -69,19 +61,49 @@ end
 # DRIVER CODE
  # initialize VirusPredictor for each state
 
+STATE_DATA.each do |state, data|
+  VirusPredictor.new(state, data[:population_density], data[:population]).virus_effects
+end 
 
-alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
-alabama.virus_effects
+# alabama = VirusPredictor.new("Alabama", STATE_DATA["Alabama"][:population_density], STATE_DATA["Alabama"][:population])
+# alabama.virus_effects
 
-jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
-jersey.virus_effects
+# jersey = VirusPredictor.new("New Jersey", STATE_DATA["New Jersey"][:population_density], STATE_DATA["New Jersey"][:population])
+# jersey.virus_effects
 
-california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
-california.virus_effects
+# california = VirusPredictor.new("California", STATE_DATA["California"][:population_density], STATE_DATA["California"][:population])
+# california.virus_effects
 
-alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
-alaska.virus_effects
+# alaska = VirusPredictor.new("Alaska", STATE_DATA["Alaska"][:population_density], STATE_DATA["Alaska"][:population])
+# alaska.virus_effects
 
 
 #=======================================================================
 # Reflection Section
+
+#1. The first hash used a string as the key then another hash as the value. 
+# The second hash uses a symbol as the key then takes a num as the value 
+
+#2. Require relative takes files from the same directory as the file your working on.
+# Where as require can take from the core ruby file. 
+
+#3. 
+  # .each 
+  # loop thru statements (if/else, while, until, begin)
+
+#4. Unnecessary repetition and using slower solutions in the methods. 
+
+#5. Making code more dry by implementing better ways of approaching a problem. 
+
+
+
+
+
+
+
+
+
+
+
+
+
